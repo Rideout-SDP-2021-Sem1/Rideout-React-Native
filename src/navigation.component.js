a25500b4a531f4b1ede2db30f1102b770f91f6bf
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2021-04-28 00:58:05 +1200

    Update import name for Login page

--- src/navigation.component.js
+++ src/navigation.component.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { LoginTest, Signup, ForgotPassword } from './auth/index';
+import { Login, Signup, ForgotPassword } from './auth/index';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const HomeNavigator = () => (
   <Navigator headerMode='none' initialRouteName="Home">
-    <Screen name='Home' component={LoginTest} />
+    <Screen name='Home' component={Login} />
     <Screen name='Signup' component={Signup} />
     <Screen name='ForgotPassword' component={ForgotPassword} />
   </Navigator>
