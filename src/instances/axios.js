a0a711fa747d04d937f58fa41996f981a77008d0
BobTheSoftwareDeveloper <bob@jd.net.nz>
2021-04-29 16:40:52 +1200

    API test call working

--- src/instances/axios.js
+++ src/instances/axios.js
@@ -2,25 +2,23 @@ import axios from 'axios'
 import { SERVER_URL } from '@env'
 import { firebase } from '@react-native-firebase/auth'
 
-console.log("SERVER_URL", SERVER_URL)
-
 const serverInstance = axios.create({
-  baseURL: SERVER_URL || "http://191.168.1.3:5000"
+  baseURL: "http://localhost:5000" || "http://139.59.120.86" || "http://191.168.1.3:5000"
 })
 
 serverInstance.interceptors.request.use(
   async (config) => {
     if (firebase.auth().currentUser != null) {
       const idTokenfinal = await firebase.auth().currentUser.getIdToken()
-      console.log("idTokenfinal", idTokenfinal)
       config.headers["auth-token"] = idTokenfinal
     }
     return Promise.resolve(config)
   },
   function (error) {
-    return Promise.reject(error);
+    console.log("error intercept request axios", error)
+    return Promise.reject(error)
   }
-);
+)
 
 export {
   serverInstance
