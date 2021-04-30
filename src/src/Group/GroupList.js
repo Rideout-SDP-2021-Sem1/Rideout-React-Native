3dc5f29f9e2c6e43937ff1d1788b2b7efb2141c3
Chickenboyray <80376080+Chickenboyray@users.noreply.github.com>
2021-04-30 13:14:04 +1200

    Update GroupList.js

--- src/src/Group/GroupList.js
+++ src/src/Group/GroupList.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Divider, List, ListItem} from '@ui-kitten/components';
+import {Avatar, Button, ListItem} from '@ui-kitten/components';
 
-const data = new Array(10).fill({
-  Group: 'Item',
-  description: 'Description for Item',
-});
+const JoinButton = props => <Button size="tiny">JoinButton</Button>;
 
-export const ListDividersShowcase = () => {
-  const renderItem = ({item, index}) => (
-    <ListItem
-      Group={`${Group.groupname} ${index + 1}`}
-      description={`${item.description} ${index + 1}`}
-    />
-  );
+function DeleteButton(props) {
+  if (Admin == true || Owner == true) {
+    <Button size="tiny">Delete</Button>;
+  }
+}
 
-  return (
-    <List
-      style={styles.container}
-      data={data}
-      ItemSeparatorComponent={Divider}
-      renderItem={renderItem}
-    />
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    maxHeight: 200,
-  },
-});
+export const GroupList = () => (
+  <ListItem
+    title="FancyGROUP"
+    accessoryRight={JoinButton}
+    accessoryLeft={DeleteButton}
+  />
+);
