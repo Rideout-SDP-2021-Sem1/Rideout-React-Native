1e558eff42c157071db297af1289d36112767a43
BobTheSoftwareDeveloper <bob@jd.net.nz>
2021-05-31 18:40:17 +1200

    Setup server ride request handler

--- server/src/socket-io/connection/handleConnection.js
+++ server/src/socket-io/connection/handleConnection.js
@@ -1,7 +1,13 @@
+const { handleRideRequest } = require("../utils")
+
 const onConnection = (io) => {
   io.on("connection", socket => {
     const uid = socket.handshake.query.uid
     console.log("New client has been connected.", uid, socket.id)
+
+    // Make this user join their own room
+    socket.join(uid)
+    console.log(socket.rooms)
 
     socket.emit("data", {
       test: "data'"
@@ -14,6 +20,9 @@ const onConnection = (io) => {
     socket.on("disconnect", () => {
       console.log("Client has disconnected.", uid, socket.id)
     })
+    
+    // Setup handlers
+    handleRideRequest(socket, uid)
   })
 }
 
